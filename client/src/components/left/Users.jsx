cd3df346bded92d6361bc02fb376a2c9dce31eea
anukalpr <singhanukalp465@gmail.com>
2025-08-27 03:18:37 +0530

    updated

--- client/src/components/left/Users.jsx
+++ client/src/components/left/Users.jsx
@@ -1,5 +1,5 @@
 import getAllUsers from "../../context/getAllusers";
-import UserContext from "../../context/userContext";
+import UserContext from "../../context/UserContext";
 import User from "./User";
 import { useState, useEffect, useContext } from "react";
 
